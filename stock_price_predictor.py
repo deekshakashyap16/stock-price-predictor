5c2637c2c3a116f2dbb6c63a63dc0dc6d81ca2c0
deekshakashyap16 <deekshakashyap05@gmail.com>
2024-10-02 22:37:29 +0530

    Update stock_price_predictor.py

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,11 +1,3 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-
 class StockPricePredictor:
     def __init__(self,csv_file):
         self.dataset=pd.read_csv(csv_file)
